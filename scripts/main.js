c78699bfbf179913e7f2dd58341374c0b0bf465c
Laurence <larrycasta11@gmail.com>
2020-06-18 14:25:56 -0500

    Configure functionality for player to mark a spot on the board

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ const gameBoard = (() => {
   for (let i = 0; i < 3; i++) {
     board.push([]);
     for (let j = 0; j < 3; j++) {
-      board[i].push('_');
+      board[i].push('');
     }
   }
   return {
@@ -15,26 +15,83 @@ const gameBoard = (() => {
 // Create factory function for players
 const Player = (name) => {
   const getName = () => name;
-  return {getName};
+  const marker  = () => (game.playerCreated) ? 'O': 'X'; // Add property indicating player's marker (X vs O)
+  const number = () => (game.playerCreated) ? 2 : 1;
+  return {
+    getName,
+    marker,
+    number,
+  };
 };
 
+// Manually create players with the factory function
+
+// Create helper factory function to determine winner
+const Winner = (board, marker) => {
+  const winningCases = () => {
+    /* let newAr = [];
+    let rowIndex = 0;
+    board.forEach(row => { // Create new array with all other markers removed
+      newAr.push([]);
+      row.forEach(spot => {
+        if (spot == marker) {
+          newAr[rowIndex].push(marker);
+        } else {
+          newAr[rowIndex].push('');
+        }
+      });
+      rowIndex++;
+    }); */
+
+
+    let newAr = board.map(row => {
+      return row.map(place => (place == marker) ? place : '');
+    });
+    return newAr;
+  }
+
+  return {
+    winningCases,
+  }
+}
+
 // Create display controller to render gameBoard
-const game = ((boardAr) => {
+const game = ((boardAr) => { 
+  let playerCreated = false // Create boolean determining if any players are created
+  let playerOneTurn = true; // Create boolean determining which player's turn it is
+  let playerTwoTurn = false;
+  
   // Create DOM elements from gameBoard array
   let container = document.querySelector('.boardContainer') // Add reference to gameBoard container node
   let board = document.createElement('table'); // Create table element for board
   let count = 0;
   boardAr.forEach(row => {
     let gameBoardRow = document.createElement('tr'); // Create table row DOM node
+    let rowIndex = boardAr.indexOf(row); // record index of row in variable
+    console.log(rowIndex);
+    let placeIndex = 0;
     row.forEach(place => { // Create a data cell for each spot
       let spot = document.createElement('td');
-      spot.textContent = (count % 2 == 0) ? 'X' : 'O'; // Create placeholders
+      spot.setAttribute('data-index', placeIndex)// Add data attribute to the DOM spot
       gameBoardRow.appendChild(spot); // Append spot to row
-      count++;
+
+      spot.addEventListener('click', (e) => { // Add event listener for each spot tied to the data attribute
+        if (spot.textContent) {return;}
+        spot.textContent = (playerOneTurn) ? 'X' : 'O'; // Add content on click
+        row[spot.getAttribute('data-index')] = spot.textContent; // change data in array after adding content
+        console.log(boardAr); // log array to console to test
+        [playerOneTurn, playerTwoTurn] = [playerTwoTurn, playerOneTurn]; // Switch players' turns
+      });
+
+      placeIndex++;
     });
     board.appendChild(gameBoardRow); // Append row to table
   });
   container.appendChild(board); // Append table to container
+
+  return {
+    playerCreated,
+  };
 })(gameBoard.board);
 
 // Test gameBoard display
