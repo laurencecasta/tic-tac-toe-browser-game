7986c0b5b43a1d3b6b62cb878b00d3ee26fa8536
Nolan Egly <nolan@nolanegly.com>
2020-09-22 17:21:46 -0700

    Display correct controls when changing between lobby and game
    I started to go off on a tangent with the lobby and players, but
    commented it all out and pivoted back to just handling the ui
    transitions for now.

--- scripts/nje.js
+++ scripts/nje.js
@@ -1,15 +1,72 @@
-const gameCoord = (() => {
+// const LobbyPlayer = (playerNum, moveChar, inputId) => {
+//     let name = document.querySelector(`input[id="${inputId}"]`).value || 
+//                 document.querySelector(`input[id="${inputId}"]`).placeholder;
+//     return {
+//         playerNum, 
+//         name,
+//         moveChar
+//     }
+// };
+
+// const lobby = (() => {
+//   let getPlayers = () =>
+//   {
+//       let p1 = new LobbyPlayer(1, "X", "playerOneInput");
+//       let p2 = new LobbyPlayer(2, "O", "playerTwoInput");
+
+//       return [p1, p2];
+//   }
+// })();
+
+// const boardGame = (() => {
+
+// })();
+
+const ui = (() => {
+  let toggleLobbyControlVisibility = () =>
+  {
+    document.getElementById('startStop').toggleAttribute('hidden');
+  };
+
+  let toggleBoardGameVisibility = () =>
+  {
+    document.getElementById('boardGameContainer').toggleAttribute('hidden');  
+  };
+
+  return {
+      toggleLobbyControlVisibility,
+      toggleBoardGameVisibility
+  }
+})();
+
+const gameCoord = ((ui) => {
   let beginMatch = () =>
   {
-      alert('beginning match');
+    ui.toggleLobbyControlVisibility();
+    ui.toggleBoardGameVisibility();
+  };
+
+  let quitMatch = () =>
+  {
+    ui.toggleLobbyControlVisibility();
+    ui.toggleBoardGameVisibility();
   };
     
   return {
-      beginMatch
+      beginMatch,
+      quitMatch
   };
-})();
+})(ui);
+
+
 
 document.querySelector('#submitNames')
   .addEventListener('click', (e) => {
     gameCoord.beginMatch();
 });
+
+document.querySelector('#quitMatch')
+  .addEventListener('click', (e) => {
+    gameCoord.quitMatch();
+});
+
