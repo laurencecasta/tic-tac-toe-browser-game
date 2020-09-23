b9b909d0207c69c1f6b84d198eb51113107214f7
Nolan Egly <nolan@nolanegly.com>
2020-09-22 19:48:47 -0700

    Add rendering of game board when starting match

--- scripts/nje.js
+++ scripts/nje.js
@@ -19,12 +19,37 @@ const ui = (() => {
       return document.querySelector(`input[id="${inputId}"]`).value || 
                   document.querySelector(`input[id="${inputId}"]`).placeholder;
     };
+
+    const renderBoard = (boardSize, spotClickCallback) => {      
+      let boardContainer = document.querySelector('.boardContainer');
+      if (boardContainer.hasChildNodes) {
+          boardContainer.removeChild(boardContainer.firstChild)
+      }
+      let board = document.createElement('table');
+      boardContainer.appendChild(board);
+      
+      let squareNumber = 0;    
+      for (let currRow = 0; currRow < boardSize; currRow++) {
+        let gameBoardRow = document.createElement('tr');
+        board.appendChild(gameBoardRow);
+        for (let currCol = 0; currCol < boardSize; currCol++) {
+          let spot = document.createElement('td');
+          spot.setAttribute('data-index', squareNumber++)
+          gameBoardRow.appendChild(spot);
+          spot.addEventListener('click', (e) => {
+            let squareId = spot.getAttribute('data-index');
+            spotClickCallback(`spot clicked on ${squareId}`);
+          });      
+        }
+      }
+    };
   
     return {
-        // toggleControlVisibilty is not meant for public use, don't include
-        toggleLobbyControlVisibility,
-        toggleBoardGameVisibility,
-        getLobbyPlayerName
+      // toggleControlVisibilty is not meant for public use, don't include
+      toggleLobbyControlVisibility,
+      toggleBoardGameVisibility,
+      getLobbyPlayerName,
+      renderBoard
     };
 })();
 
@@ -50,40 +75,61 @@ const lobby = ((ui) => {
   }
 })(ui);
 
-const gameCoord = ((lobby, ui) => {
+const board = ((ui) => {
+  let boardSize = 0;
+  let board = new Array(boardSize);
+
+  const initialize = (boardSize) => {
+    boardSize = boardSize;
+    board = new Array(boardSize).fill('').map(() => new Array(boardSize).fill(''));
+  };
+
+  const onSpotClick = (spotClicked) => {
+    alert(spotClicked);
+  };
+  
+  return {
+    initialize,
+    onSpotClick
+  }; 
+
+})(ui);
+
+const gameCoord = ((lobby, board, ui) => {
 
   const toggleBetweenLobbyAndBoardGame = () => {
     ui.toggleLobbyControlVisibility();
     ui.toggleBoardGameVisibility();  
   };
 
-  const beginMatch = () => {
-      let players = lobby.getPlayers();
-      // set up board
-      toggleBetweenLobbyAndBoardGame();
+  const onBeginMatch = () => {
+    board.initialize(3);
+    let players = lobby.getPlayers();      
+    ui.renderBoard(3, board.onSpotClick);
+    toggleBetweenLobbyAndBoardGame();
   };
 
-  const quitMatch = () =>
+  const onQuitMatch = () =>
   {
-      toggleBetweenLobbyAndBoardGame();
+    toggleBetweenLobbyAndBoardGame();
   };
     
   return {
-      // toggleBetweenLobbyAndBoardGame not meant for public use, don't include
-      beginMatch,
-      quitMatch
+    // toggleBetweenLobbyAndBoardGame not meant for public use, don't include
+    onBeginMatch,
+    onQuitMatch
   };
-})(lobby, ui);
+})(lobby, board, ui);
 
 
 // wire up UI elements to game coordinator
 document.querySelector('#submitNames')
   .addEventListener('click', (e) => {
-    gameCoord.beginMatch();
+    gameCoord.onBeginMatch();
 });
 
 document.querySelector('#quitMatch')
   .addEventListener('click', (e) => {
-    gameCoord.quitMatch();
+    gameCoord.onQuitMatch();
 });
 
